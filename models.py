cd723722643453069aa3e43ea0d857a40b1ff72e
Shivam-sah304 <shivma_081bct039@tcioe.edu.np>
2026-02-11 23:33:22 +0545

    working on doctor form

--- models.py
+++ models.py
@@ -3,21 +3,21 @@ from datetime import datetime
 db = SQLAlchemy()
 
 class Doctor(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, nullable=False)
     name=db.Column(db.String(50),nullable=False)
     email=db.Column(db.String(40),nullable=False)
     address=db.Column(db.String(50),nullable=False)
-    phone=db.Column(db.String(20),nullable=False)
+    phone=db.Column(db.String(20),primary_key=True)
     degree=db.Column(db.String(30),nullable=False)
     specialization=db.Column(db.String(40),nullable=True)
     desc=db.Column(db.String(400),nullable=True)
     dob=db.Column(db.Date,nullable=False)
     experience=db.Column(db.Integer,nullable=False)
 
-    # password=db.Column(db.String(30),nullable=False)
-    # photo=db.Column(db.String(40),nullable=False)
-    # nmc_number=db.Column(db.String(50),nullable=False)
-    # nmc_photo=db.column(db.string(100),nullable=False)
+    password=db.Column(db.String(30),nullable=True)
+    photo=db.Column(db.String(40),nullable=True)
+    nmc_number=db.Column(db.String(50),nullable=True)
+    nmc_photo=db.Column(db.String(100),nullable=True)
 
     
     
