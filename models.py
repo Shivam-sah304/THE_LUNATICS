5b32da41a64a48f3414722b04a13e39caf49e1b1
Shivam-sah304 <shivma_081bct039@tcioe.edu.np>
2026-02-11 19:39:48 +0545

    adding doctor registration

--- models.py
+++ models.py
@@ -10,11 +10,15 @@ class Doctor(db.Model):
     phone=db.Column(db.String(20),nullable=False)
     degree=db.Column(db.String(30),nullable=False)
     specilization=db.Column(db.String(40),nullable=False)
-    password=db.Column(db.String(30),nullable=False)
-    photo=db.Column(db.String(40),nullable=False)
-    nmc_number=db.Column(db.String(50),nullable=False)
-    nmc_photo=db.column(db.string(100),nullable=False)
-    
+    desc=db.Column(db.String(400),nullable=False)
+    dob=db.Column(db.Date,nullable=False)
+    experience=db.Column(db.Integer,nullable=False)
+
+    # password=db.Column(db.String(30),nullable=False)
+    # photo=db.Column(db.String(40),nullable=False)
+    # nmc_number=db.Column(db.String(50),nullable=False)
+    # nmc_photo=db.column(db.string(100),nullable=False)
+
     def __init__(self) ->str:
         return f"{self.id}-{self.name}"
     
--- routes.py
+++ routes.py
@@ -12,3 +12,17 @@ routes = Blueprint("routes", __name__)
 @routes.route("/")
 def home():
     return render_template("index.html")
+
+@routes.route("/doctorregistration",methods=["GET","POST"])
+def doctorregistration():
+    if request.method=="POST":
+        name=request.form["full_name"]
+        email=request.form["email"]
+        phone=request.form["phone"]
+        address=request.form["address"]
+        dob=datetime.strptime(request.form["dob"], "%Y-%m-%d").date()
+        desc=request.form["about"]
+        degree=request.form["education"]
+        experience=request.form["experience"]
+        
+
