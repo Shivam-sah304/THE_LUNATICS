5a24042171e11a392f55d94b74ffa4dfb447d9e6
Shivam-sah304 <shivma_081bct039@tcioe.edu.np>
2026-02-11 19:20:56 +0545

    making table in the models.py

--- models.py
+++ models.py
@@ -7,6 +7,14 @@ class Doctor(db.Model):
     name=db.Column(db.String(50),nullable=False)
     email=db.Column(db.String(40),nullable=False)
     address=db.Column(db.String(50),nullable=False)
+    phone=db.Column(db.String(20),nullable=False)
+    degree=db.Column(db.String(30),nullable=False)
+    specilization=db.Column(db.String(40),nullable=False)
+    password=db.Column(db.String(30),nullable=False)
+    photo=db.Column(db.String(40),nullable=False)
+    nmc_number=db.Column(db.String(50),nullable=False)
+    nmc_photo=db.column(db.string(100),nullable=False)
+    
     def __init__(self) ->str:
         return f"{self.id}-{self.name}"
     
